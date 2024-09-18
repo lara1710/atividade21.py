7e0d79eb64c6e6bb67bdb6e977a2ada9583f1403
lara1710 <laraleticia1203@gmail.com>
2024-09-18 16:04:10 -0300

    FIM!

---
+++ atividade21.py
@@ -0,0 +1,9 @@
+# Crie um programa que faça uma contagem regressiva de 10 até 0 e exiba "FIM!" ao final
+
+sequencia = 0
+
+for sequencia in range(11):
+    sequencia - 1
+    print(sequencia)
+
+print('FIM!')
